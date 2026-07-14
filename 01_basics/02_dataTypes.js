192fac75c2a0437142861596925d459a42abd6a1
Askaran Patil <askpatil.21@gmail.com>
2026-07-14 02:19:24 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+
+let name = "Askaran"
+let age = 20
+let isLoggedIn = false
+
+// number- 2 to power 53
+// bigint
+// string= ""
+// boolean= true/false
+// null= standalone value
+// undefined
+// symbol= unique
+// object
+
+console.log(typeof name);
+
