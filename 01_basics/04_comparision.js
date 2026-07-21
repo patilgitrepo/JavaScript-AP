4c325bd746f0c5cb6bdd8301c763d7ed49ed34be
Askaran Patil <askpatil.21@gmail.com>
2026-07-21 02:20:19 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//  notes
+// comparision and equality are two differt things
+
+// === (checks)
+console.log("2" === 2);
